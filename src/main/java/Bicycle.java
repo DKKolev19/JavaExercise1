00a351cf7915951d78fa23b2b02b70a2a7e6cbd2
Daniel Kolev <dkkolev19@codingburgas.bg>
2022-11-11 12:47:59 +0200

    Add printMatrix method and Bicycle.java class

---
+++ src/main/java/Bicycle.java
@@ -0,0 +1,4 @@
+public class Bicycle {
+    int weight;
+    String model;
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
 import java.util.*;
 public class Main {
-    public static void main(String []args)
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        printMatrix(n);
+    }
+    public static void printMatrix(int n)
     {
 
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print((int)(Math.random() * 2) + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
